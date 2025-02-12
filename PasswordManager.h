bb1e408e7a58faef5f54e79861caf70284650e17
Sahil Sobhani <sahilsobhani4@gmail.com>
2025-02-12 16:16:03 +0530

    added comments

--- PasswordManager.h
+++ PasswordManager.h
@@ -1,12 +1,16 @@
 
-
+// Macros
 #ifndef PASSWORDMANAGER_H
 #define PASSWORDMANAGER_H
 
+// Header imports
 #include <string>
 #include <vector>
 
 using namespace std;
+
+//Structure for a single password entry
+
 struct PasswordEntry {
 
 string account;
@@ -14,6 +18,8 @@ string username;
 string password;
 
 };
+
+// Monolith class for the password manager
 
 class PasswordManager{
 
@@ -23,19 +29,25 @@ private:
     string filename;
     string masterPassword;
     
+    //Simple XOR encryption and decryption function for password in data file
     string xorEncryptDecrpyt(const string &data, const string key);
 
 public: 
 
+    // Constructor to create a password manager
     PasswordManager(const string &file, const string &masterPwd);
+    
+    // Function to load and save entries in a file
     bool loadEntries();
     bool saveEntries();
 
+    // CRUD operations - Functions on a particular entry
     void addEntry(const string &account, const string &username, const string &password);
     void listEntry() const;
     bool retrieveEntry(const string &account , PasswordEntry &entry) const;
     bool deleteEntry(const string &account);
 
+    // Function to generate a password
     string generatePassword(int length) const;
 };
 
